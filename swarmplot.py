95cebb3633ee902bf9437aac759b85761d24c65f
rodrigozuniga <rodrigozuniga@gmail.com>
2018-08-13 15:08:38 -0700

    add Tukey;s quote

--- swarmplot.py
+++ swarmplot.py
@@ -20,3 +20,7 @@ print(os.getcwd())
 plt.savefig("swarmplot_iris.pdf", transparent = True)
 
 plt.show()
+
+# """Exploratory data analysis can never be the
+# whole story, but nothing else can serve as
+# the foundation stone.  --John Tukey"""
